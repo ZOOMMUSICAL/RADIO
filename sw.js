5e3eaf7244fec6f1def2e39fee65249fc74e7d38
ZOOMMUSICAL <zoomradionline@gmail.com>
2025-09-25 12:14:03 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,71 +1,53 @@
-//asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_zoom',
-  urlsToCache = [
-    './',
-    'http://ibizaglobalradio.streaming-pro.com:8024/;?type=http&nocache=14583',
-    'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css',
-    'https://img.icons8.com/ios-filled/50/ffffff/vinyl.png',
-  'https://blogger.googleusercontent.com/img/a/AVvXsEhobSdCEj3wpR-qMr1nbwVmfqnbyo8Tzx8iMirm4BnQmfNq41VrFlBMnH_H86fzI3SSSEyBGmzJGdm1Y5Bgi0p6RDRbn3BDHB3cYLWgEBqV-txWdrfOCWf9b113fbYkoFDFfSvB0y97WZluGsJvtCQAjFP8oAn9jm2Rlgc7-wtE9ikn3OrOQXtC69IYObo=s600',
-  'https://stream.zeno.fm/dfxxkhvkxs8uv?cb=12345',
-  'https://img.icons8.com/ios-filled/50/ffffff/low-volume.png',
-  'https://img.icons8.com/ios-filled/50/ffffff/no-audio--v1.png',
-  'https://img.icons8.com/ios-filled/50/ffffff/medium-volume.png',
-  'https://img.icons8.com/ios-filled/50/ffffff/high-volume.png'
-    './style.css',
-    'https://code.jquery.com/jquery-3.2.1.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js',
-    './script.js',
-    './img/logo.png',
-    './favicon.ico',
-    './img/favicon.png'
-  ]
+const CACHE_NAME = 'v1_zoom_v2';
+const urlsToCache = [
+  './',
+  './style.css',
+  './script.js',
+  './img/logo.png',
+  './favicon.ico',
+  './img/favicon.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css',
+  'https://code.jquery.com/jquery-3.2.1.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js'
+];
 
-//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+// ===== Instalación: cache solo archivos esenciales =====
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting())
-      })
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
       .catch(err => console.log('Falló registro de cache', err))
-  )
-})
+  );
+});
 
-//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+// ===== Activación: limpiar caches antiguos =====
 self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME]
-
+  const cacheWhitelist = [CACHE_NAME];
   e.waitUntil(
     caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName)
-            }
-          })
-        )
-      })
-      // Le indica al SW activar el cache actual
+      .then(keys => Promise.all(
+        keys.map(key => {
+          if (!cacheWhitelist.includes(key)) return caches.delete(key);
+        })
+      ))
       .then(() => self.clients.claim())
-  )
-})
+  );
+});
 
-//para cuando el navegador recupera una url
+// ===== Fetch: archivos estáticos por cache, el resto directo a internet =====
 self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
-  e.respondWith(
-    caches.match(e.request)
-      .then(res => {
-        if (res) {
-          //recuperar cache
-          return res
-        }
-        //recuperar de la petición a la url
-        return fetch(e.request)
-      })
-  )
-})
+  const requestUrl = new URL(e.request.url);
+
+  // Solo cacheamos los archivos esenciales de la PWA
+  if (urlsToCache.includes(requestUrl.pathname) || urlsToCache.includes(e.request.url)) {
+    e.respondWith(
+      caches.match(e.request)
+        .then(res => res || fetch(e.request))
+    );
+  } else {
+    // TV en vivo, radio streaming y videos: siempre online
+    e.respondWith(fetch(e.request));
+  }
+});
